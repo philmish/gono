e45bd793648f904310c40addd840039086792f7e
philmish <thantalosdream@protonmail.com>
2022-10-01 00:39:12 +0200

    feat: add create func for project, topic and subtopic

--- database/models/subtopic.go
+++ database/models/subtopic.go
@@ -12,3 +12,22 @@ type Subtopic struct {
     Lists []List `gorm:"foreignKey:SubtopicID;references:ID"`
     Images []Image `gorm:"foreignKey:SubtopicID;references:ID"`
 }
+
+func CreateSubtopic(name string, toid int, db *gorm.DB) error {
+    var topic Topic
+    err := db.First(&topic, toid).Error
+    if err != nil {
+        return err
+    }
+    subtopic := Subtopic{
+        Name: name,
+        TopicID: int(topic.ID),
+        Notes: []Note{},
+        Texts: []Text{},
+        Links: []Link{},
+        Lists: []List{},
+        Images: []Image{},
+    }
+    err = db.Create(&subtopic).Error
+    return err
+}
--- database/models/topic.go
+++ database/models/topic.go
@@ -10,3 +10,20 @@ type Topic struct {
     Project Project `gorm:"foreignkKey:ProjectID"`
     Subtopics []Subtopic `gorm:"foreignKey:TopicID;references:ID"`
 }
+
+func CreateTopic(name, desc string, prid int, db *gorm.DB) error {
+    var project Project
+    err := db.First(&project, prid).Error
+    if err != nil {
+        return err
+    }
+    topic := Topic{
+        Name: name,
+        Description: desc,
+        ProjectID: int(project.ID),
+        Project: project,
+        Subtopics: []Subtopic{},
+    }
+    err = db.Create(&topic).Error
+    return err
+}
